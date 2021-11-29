fca481965b4666fb732dfeaa629804a60184796d
Jeff Xie <xiehuan09@gmail.com>
2021-11-29 14:23:05 +0800

    tracing/README: Document objtrace trigger syntax
    Signed-off-by: Jeff Xie <xiehuan09@gmail.com>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -5578,6 +5578,9 @@ static const char readme_msg[] =
 	"\t            enable_hist:<system>:<event>\n"
 	"\t            disable_hist:<system>:<event>\n"
 #endif
+#ifdef CONFIG_TRACE_OBJECT
+	"\t            objtrace:add:obj[,offset][:type][:count][if <filter>]\n"
+#endif
 #ifdef CONFIG_STACKTRACE
 	"\t\t    stacktrace\n"
 #endif
